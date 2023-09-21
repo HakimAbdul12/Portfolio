cea8021dcb8a6f107103444918ceb92f8a27582d
hakim <hakim@gmail.com>
2023-09-21 17:28:53 -0100

    new change

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
   return (
     <div id='about' className='h-[70vh] w-full bg-violet-500 text-4xl flex justify-center items-center'>
-      About
+      My name is Hakim
     </div>
   )
 }
